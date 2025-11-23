20f53b1cddbc6367aecb171b48235138fd1d5b2f
Zero123 <11438691+bili_zero123@user.noreply.gitee.com>
2025-11-23 18:09:34 +0800

    增加实验性可选模块

---
+++ Optional/Modules/EventRegistry/ServerBus.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from functools import wraps
+from ...Systems.Loader.Server import LoaderSystem as _LoaderSystem
+
+def _parseEventClass(func):
+    return getattr(func, "func_defaults")[0].__class__
+
+def SubscribeEvent(func):
+    """ 面向对象事件监听装饰器 """
+    eventCls = _parseEventClass(func)
+    @wraps(func)
+    def _wrapperFunc(args=None):
+        return func(eventCls(args))
+    _LoaderSystem.REG_STATIC_LISTEN_FUNC(eventCls.__name__, _wrapperFunc)
+    return _wrapperFunc
